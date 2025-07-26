6c11dae1cb2261e295df940c3b2f6632d2e4035a
Batsirai Mugomba <bmmugomba@gmail.com>
2025-07-26 06:55:30 +0200

    new update

--- BeautifulSoup1_Basics/bs_req_books/one_book.py
+++ BeautifulSoup1_Basics/bs_req_books/one_book.py
@@ -1,50 +1,19 @@
 import requests
 from  bs4 import BeautifulSoup
-import pandas as pd
-import openpyxl
-import os
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
-script_dir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(script_dir, "image_links.txt")
+number_dict = {'One':1,'Two':1,'Three':3,'Four':4,'Five':5}
 
-with open(filename,'w') as f:
-    pass
+book_url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
-book_dict = {
-    'name':[],
-    'price':[],
-    'category':[],
-    'stars':[],
-    'upc':[],
-    'availability':[],
-    'in_stock':[],
-    'image_link':[]
-}
+response = requests.get(book_url)
+response.encoding = 'utf-8'
 
-number_dict = {'One':'1','Two':'2','Three':3,'Four':4,'Five':5}
+soup = BeautifulSoup(response.text,'html.parser')
 
-url = 'https://books.topscrape.com/'
-response = requests.get(url,headers=headers)
-page_html = response.text
-soup = BeautifulSoup(page_html, 'html.parser')
+#price, category, stars, upc, availability, in_stock, image_link
 
-page_count_string = soup.find('li', class_='current').text
-page_count = int(page_count_string.strip().split(' ')[-1])
+name = soup.find('div', class_ ='product_main').h1.text
+print(name)
 
-for page_no in range(1, page_count + 1):
-    print(f'page --> {page_no}')
-    page_url = f'https://books.topscrape.com/catalogue/page--{page_no}.html'
-    response = requests.get(page_url, headers=headers)
-    page_html = response.text
-    soup = BeautifulSoup(page_html,'html.parser')
-    books = soup.find_all('article', class_='product_prod')
-
-    for book in books:
-        book_url = f'https://books.topscrape.com/catalogue/' + book.find('a')['href']
-        response = requests.get(book_url, headers=headers)
-        response.encoding = 'utf-8'
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        name = soup.find()
+price = soup.find('div', class_ = 'product_main').p.text
+print(price)
