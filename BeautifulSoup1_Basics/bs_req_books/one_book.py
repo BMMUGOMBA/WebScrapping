6e81127b16831ff3247cb8ff1aabb1ad28df09c6
Batsirai Mugomba <bmmugomba@gmail.com>
2025-07-26 07:17:20 +0200

    one done

--- BeautifulSoup1_Basics/bs_req_books/one_book.py
+++ BeautifulSoup1_Basics/bs_req_books/one_book.py
@@ -17,3 +17,30 @@ print(name)
 
 price = soup.find('div', class_ = 'product_main').p.text
 print(price)
+
+ul_container = soup.find('ul', class_='breadcrumb')
+li_items = ul_container.find_all('li')
+category = li_items[2].a.text
+print(category)
+
+star_p_element = soup.find('p', class_ = 'star-rating')
+star_class_name_list = star_p_element['class']
+star_string = star_class_name_list[1]
+stars = number_dict[star_string]
+print(stars)
+
+upc_th = soup.find('th', string = 'UPC')
+upc = upc_th.find_next_sibling().text
+print(upc)
+
+availability_th = soup.find('th', string = 'Availability')
+availability = availability_th.find_next_sibling().text
+print(availability)
+
+#In stock (22 available)
+#22 Available
+in_stock = availability.split('(')[1].split(' ')[0]
+print(in_stock)
+
+image_link = 'https://books.toscrape.com/' + soup.find('div', class_='thumbnail').img['src'][6:]
+print(image_link)
