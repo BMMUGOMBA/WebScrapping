818da3b895fc581e56c4a56d6cb7d890d8204145
Batsirai Mugomba <bmmugomba@gmail.com>
2025-07-28 19:16:22 +0200

    added def

--- Selenium/login.py
+++ Selenium/login.py
@@ -2,6 +2,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def login(the_driver):
+    login_button = driver.find_element(By.XPATH, value='//a[text()="Login"]')
+    login_button.click()
+
+    username_input = driver.find_element(By.ID, value='username')
+    username_input.send_keys('myusername')
+
+    password_input = driver.find_element(By.ID, value='password')
+    password_input.send_keys('123456')
+
+    log_me_in = driver.find_element(By.CSS_SELECTOR, value = 'input[value = "Login"]')
+    log_me_in.click()
+
 url = 'https://quotes.toscrape.com/js/'
 
 options = webdriver.ChromeOptions()
@@ -12,14 +25,4 @@ options.add_experimental_option( name='detach',value=True)
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 
-login_button = driver.find_element(By.XPATH, value='//a[text()="Login"]')
-login_button.click()
-
-username_input = driver.find_element(By.ID, value='username')
-username_input.send_keys('myusername')
-
-password_input = driver.find_element(By.ID, value='password')
-password_input.send_keys('123456')
-
-log_me_in = driver.find_element(By.CSS_SELECTOR, value = 'input[value = "Login"]')
-log_me_in.click()
+login(driver)
