39c9016f5c2bd8e18979308b2966228a83ba19ae
Batsirai Mugomba <bmmugomba@gmail.com>
2025-07-28 19:58:48 +0200

    just all to be safe lets go home

---
+++ Selenium/scrapping1.py
@@ -0,0 +1,36 @@
+#Creating Driver and opening driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def login(the_driver):
+    login_button = driver.find_element(By.XPATH, value='//a[text()="Login"]')
+    login_button.click()
+
+    username_input = driver.find_element(By.ID, value='username')
+    username_input.send_keys('myusername')
+
+    password_input = driver.find_element(By.ID, value='password')
+    password_input.send_keys('123456')
+
+    log_me_in = driver.find_element(By.CSS_SELECTOR, value = 'input[value = "Login"]')
+    log_me_in.click()
+
+url = 'https://quotes.toscrape.com/js/'
+
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+
+options.add_experimental_option( name='detach',value=True)
+
+driver = webdriver.Chrome(options=options)
+driver.get(url)
+
+login(driver)
+
+quotes = driver.find_elements(By.CLASS_NAME, value='quote')
+print(len(quotes))
+
+#quote,author, tags
+for element in quotes:
+    quote = element.find_element(By.CSS_SELECTOR, value = 'span.text').text
+    print(quote)
